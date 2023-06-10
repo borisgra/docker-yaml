91fa922e607cdce14ef88116bcfe77b9a82afa50
boris_gra <boris.b@smartcomfort.solution>
2023-06-10 09:41:16 +0300

    install_docker.sh 3

--- install_docker.sh
+++ install_docker.sh
@@ -5,6 +5,6 @@ sudo bash -c "$(curl -fsSL https://get.docker.com)" #https://docs.docker.com/eng
 sudo apt update && sudo apt install nano && sudo apt install -y git #  Install dependencies
 sudo git clone https://github.com/borisgra/docker-yaml.git --branch main
 sudo chmod 777 docker-yaml/config*
-cd ~/docker-yaml/yamls
+cd docker-yaml/yamls
 
 sudo docker compose up # pgsql:version+pgAdmin4:last+odoo:version  (param in .env)
