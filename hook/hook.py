54ce255fab09a5ec3bc3b46b6ae0cf0051204498
boris_gra <boris.b@smartcomfort.solution>
2023-06-21 10:47:40 +0300

    flask -> hook

--- hook/hook.py
+++ hook/hook.py
@@ -1,6 +1,8 @@
 import sys
 from flask import Flask, render_template, request
 import os
+import hashlib
+import hmac
 
 app = Flask(__name__)
 
@@ -9,7 +11,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app. route('/hooktest', methods=['GET', 'POST'])
+
+@app.route('/hook', methods=['GET', 'POST'])
 def hook_root():
     print("webhook headers:")
     print(request.headers)
@@ -21,26 +24,23 @@ def hook_root():
     else:
         return 'ERROR', 400
 
-import hashlib
-import hmac
+
 def verify_signature(payload_body, secret_token, signature_header):
     """Verify that the payload was sent from GitHub by validating SHA256
     https://docs.github.com/en/webhooks-and-events/webhooks/securing-your-webhooks.
-
-    Raise and return 403 if not authorized.
-
     Args:
-        payload_body: original request body to verify (request.body())
+        payload_body: original request body to verify (request.data)
         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
         signature_header: header received from GitHub (x-hub-signature-256)
     """
     if not signature_header:
-        return  403, "x-hub-signature-256 header is missing!"
+        return 403, "x-hub-signature-256 header is missing!"
     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
     if not hmac.compare_digest(expected_signature, signature_header):
         return 403, "Request signatures didn't match!"
-    return "OK",200
+    return "OK", 200
+
 
 if __name__ == "__main__":
     port = os.getenv('PORT', 5003)
