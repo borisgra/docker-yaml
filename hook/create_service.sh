af4c68b8d4287afa2b57ae774eec2295ffe958af
boris_gra <boris.b@smartcomfort.solution>
2023-06-27 19:38:43 +0300

    help.html

--- hook/create_service.sh
+++ hook/create_service.sh
@@ -2,7 +2,6 @@
 #sudo systemctl stop hook.service
 #sudo systemctl status hook.service
 
-
 sudo chmod +x ./webhook_listener.sh
 sudo cp webhook_listener.sh /usr/bin && sudo cp hook.service /lib/systemd/system
 sudo systemctl daemon-reload # reload this daemon each time after making any changes in .service file
