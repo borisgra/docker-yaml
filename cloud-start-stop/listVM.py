d2834670781d12b14be0fd06f1078a273a4e5de8
boris_gra <boris.b@smartcomfort.solution>
2024-11-18 18:21:04 +0200

    request.url.repl.replace('/list','')

--- cloud-start-stop/listVM.py
+++ cloud-start-stop/listVM.py
@@ -1,40 +1,44 @@
 import functions_framework
 import requests
-from requests.structures import CaseInsensitiveDict
 from getToken import getToken
 
 @functions_framework.http
 def listVM(request):
-    com = 'start'
-    vm = '????'
     project = '????'
     zone = 'us-central1-a'
     resp = request.get_json(silent=True)
     if not resp:
         resp = request.args
     if resp:
-        if 'vm' in resp:
-            vm = resp['vm']
         if 'project' in resp:
             project = resp['project']
         if 'zone' in resp:
             zone = resp['zone']
 
     token = getToken()
-
     url = ("https://compute.googleapis.com/compute/v1/projects/{}/zones/{}/instances"
            .format(project,zone))
     print(url)
-    headers = CaseInsensitiveDict()
-    headers["Authorization"] = "Bearer {}".format(token)
-    headers["Accept"] = "application/json"
-    headers["Content-Length"] = "0"
-    resp = requests.post(url, headers=headers)
-    print(resp.status_code)
-    items = resp.json()['items']
-    vmList=""
-    for item in items:
-        vm = items[item]
-        vmList += "{}  {} \n".format(vm.name,vm.status)
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+        'Accept': 'application/json',
+    }
+    params = {
+        'key': 'null',
+    }
+    response = requests.get(
+        url,
+        params=params,
+        headers=headers,
+    )
 
-    return 'List VM ({} \n {})'.format(com,resp.status_code,vmList)
+    vmList=""
+    urlCom = request.url.replace('/list','')
+    if not response.status_code == 200:
+        return 'List! ({})'.format(response.status_code)
+    else:
+        items = response.json()['items']
+        for item in items:
+            vmList += "<b>{}  {} <a href='{}&vm={}&com=start'> START </a> .. <a href='{}&vm={}&com=stop'> STOP </a>   <br> ".format(item['name'],item['status'],urlCom,item['name'],urlCom,item['name'])
+
+    return 'List VM ({}) <br><br> {}'.format(response.status_code,vmList)
