35b908e01e8e6cae35f2141374ec9f96d4bb6af7
boris_gra <boris.b@smartcomfort.solution>
2024-12-17 15:14:35 +0200

    all zones in project

--- cloud-start-stop/listVM.py
+++ cloud-start-stop/listVM.py
@@ -10,10 +10,8 @@ def listVM(request):
     urlCom = request.url.split('?')[0].replace('http:','https:')  # todo http ??
     print('urlCom =',urlCom)
     com, projects, vm, zone = get_param(request)
-    if zone == '':
-        return 'Add param zone=  (url?zone=my_zone) - by default zone=us-central1-a'
-    elif projects == '':
-        return 'Add param projects=  (url?projects=my_projects_id or url?projects=my_projects1_id,my_project2_id)'
+    if projects == '':
+        return 'Add param (url?projects=my_projects1_id,my_project2_id,..)'
 
     vmList=""
     codes=[]
@@ -26,13 +24,14 @@ def listVM(request):
     for project  in projects.split(","):
         codes, vmList = one_project(codes, project, token, urlCom, vmList, zone)
 
-    vmList = '<b><table>{}</table></b>'.format(vmList)
+    vmList = ('<b><table> <th>Project</th> <th>Name VM</th> <th>Zone</th> <th>Status</th> <th>Start</th> <th>Stop</th> <th>natIP</th>\n '
+              '{}</table></b>').format(vmList)
     return render_template('index.html',codes=','.join(codes),data=vmList,projects=projects)
 
 
 def one_project(codes, project, token, urlCom, vmList, zone):
-    url = ("https://compute.googleapis.com/compute/v1/projects/{}/zones/{}/instances"
-           .format(project, zone))
+    # url = ("https://compute.googleapis.com/compute/v1/projects/{}/zones/{}/instances"  # list - one zone
+    url = "https://compute.googleapis.com/compute/v1/projects/{}/aggregated/instances".format(project) # aggregatedList - all zones
     print(url)
     headers = {
         'Authorization': "Bearer {}".format(token),
@@ -50,15 +49,23 @@ def one_project(codes, project, token, urlCom, vmList, zone):
         if 'items' in response.json():
             items = response.json()['items']
             for item in items:
-                accessConfigs = item['networkInterfaces'][0]['accessConfigs'][0]
-                natIP = accessConfigs['natIP'] if 'natIP' in accessConfigs else ''
-                vmList += ('<tr> <td>{}</td> <td>{}</td>  <td>{}</td> \n'
-                           '<td> <button onclick="com_vm(\'{}?vm={}&com=start&projects={}&zone={}\')">&nbsp;START</button> </td> \n'
-                           '<td> <button onclick="com_vm(\'{}?vm={}&com=stop&projects={}&zone={}\')">&nbsp;STOP</button> </td> \n'
-                           '<td> {}</td></tr> '
-                           .format(project, item['name'], item['status'],
-                                   urlCom, item['name'], project, zone,
-                                   urlCom, item['name'], project, zone
-                                   , natIP))
+                if 'instances' in items[item]:
+                    instances = items[item]['instances']
+                    for instance in instances:
+                        name = instance['name']
+                        status = instance['status']
+                        zone = instance['zone'].split('/')[-1]
+                        accessConfigs = instance['networkInterfaces'][0]['accessConfigs'][0]
+                        natIP = accessConfigs['natIP'] if 'natIP' in accessConfigs else ''
+
+                        vmList += ('<tr> '
+                                   '<td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> \n'
+                                   '<td> <button onclick="com_vm(\'{}?vm={}&com=start&projects={}&zone={}\')">&nbsp;START</button> </td> \n'
+                                   '<td> <button onclick="com_vm(\'{}?vm={}&com=stop&projects={}&zone={}\')">&nbsp;STOP</button> </td> \n'
+                                   '<td> {}</td></tr> '
+                                   .format(project, name, zone, status,
+                                           urlCom,name, project, zone,
+                                           urlCom, name, project, zone
+                                           , natIP))
     codes.append(str(response.status_code))
     return codes, vmList
