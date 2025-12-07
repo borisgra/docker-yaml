7b7563dd967ea52e5b10b5cc059f52d2db680fef
boris_gra <boris.b@smartcomfort.solution>
2025-12-07 15:08:13 +0200

    -f compose-query.yaml --env-file .env_query

--- install_query.sh
+++ install_query.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+sudo apt update
+sudo apt install net-tools  # ifconfig
 echo "install docker -----------------------"
 sudo bash -c "$(curl -fsSL https://get.docker.com)"
 echo "installed docker !!! -----------------------"
 curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/compose-query.yaml -O
-curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/.env_query > .env
+curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/compose-bd.yaml -O
+curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/.env_query > .env_query
+curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/.env - O
+sudo mkdir config_pgadmin4
+sudo chmod 777 config*
 echo "install query , loadmenu , computers-start-stop"
-sudo docker compose -f compose-query.yaml up
+sudo docker compose -f compose-query.yaml --env-file .env_query up
+#sudo docker compose -f compose-bd.yaml --env-file .env up
