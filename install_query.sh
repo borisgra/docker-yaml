31571d64432b4245031b177119166d47ccaa922d
boris_gra <boris.b@smartcomfort.solution>
2025-12-09 09:42:02 +0200

    borisgra/greed:latest  # with Python

--- install_query.sh
+++ install_query.sh
@@ -9,7 +9,10 @@ curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/
 curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/.env_query > .env_query
 curl -fsSL https://raw.githubusercontent.com/borisgra/docker-yaml/develop/yamls/.env - O
 sudo mkdir config_pgadmin4
-sudo chmod 777 config*
+sudo mkdir config_pgadmin4/storage
+sudo mkdir config_pgadmin4/storage/mail_gmail.com
+sudo chmod 777 -R config*
 echo "install query , loadmenu , computers-start-stop"
 sudo docker compose -f compose-query.yaml --env-file .env_query up
-#sudo docker compose -f compose-bd.yaml --env-file .env up
+sudo docker compose -f compose-bd.yaml --env-file .env up
+#sudo apt install certbot python3-certbot-nginx -y
